8040d81b7a623963d0122f26691df7a3e2791af5
Adnan Bayadwala <cadbay53@gmail.com>
2024-12-11 23:47:02 +0530

    debug:UI

--- src/components/ClassCard.jsx
+++ src/components/ClassCard.jsx
@@ -24,7 +24,7 @@ export default function ClassCard({
   }
   return (
     <>
-      <div className="flex flex-col bg-blue-200 shadow-md rounded-lg w-96 mx-auto md:mx-10 md:mt-10 mt-7">
+      <div className="flex flex-col bg-blue-200 shadow-md rounded-lg w-[22rem] mx-auto md:mx-10 md:mt-10 mt-7">
         <div className="h-56 m-2.5 text-white  overflow-hidden rounded-md">
           <img src={card} alt="card-image" className="brightness-90" />
         </div>
@@ -47,23 +47,23 @@ export default function ClassCard({
             </h6>
           )}
         </div>
-        <div className="px-4 pb-4 pt-0 mt-2 mx-auto">
+        <div className="px-3 pb-4 pt-0 mt-2 mx-auto">
           <button
-            className="rounded-md bg-slate-800 py-2 px-4 mr-3 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-md bg-slate-800 py-2 px-3 mr-3 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
             onClick={() => onClickStart(id)}
           >
             Start Daur
           </button>
           <button
-            className="rounded-md bg-slate-800 py-2 px-4 mr-3 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-md bg-slate-800 py-2 px-3 mr-3 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
             onClick={() => onClickEdit(id)}
           >
             Edit Daur
           </button>
           <button
-            className="rounded-md bg-slate-800 py-2 px-4 mr-3 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-md bg-slate-800 py-2 px-3 mr-3 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
             onClick={() => onClickDelete(id)}
           >
